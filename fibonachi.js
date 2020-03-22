6a2f312d5cc9e3d2eecfd2706a35702e11232bfe
Nagoluk <51786414+Nagoluk@users.noreply.github.com>
2020-03-22 13:50:32 +0200

    fibonachi

---
+++ fibonachi.js
@@ -0,0 +1,21 @@
+//Naholiuk Dmitriy 22.03.2020 13:31
+
+
+//Натуральное число N является числом Фибоначчи тогда и только тогда, когда 5N^2 + 4 или 5N^2 - 4 
+//является квадратом. Квадратное число — число, являющееся квадратом некоторого целого числа.
+
+let isFibonachi = num => {
+    const temp = 5 * (num**2);
+
+
+    if(Math.sqrt(temp - 4)%1 === 0 || Math.sqrt(temp + 4)%1 === 0 ){
+       return true
+    }
+
+    return false;
+}
+
+console.log(isFibonachi(10946)); //true
+console.log(isFibonachi(2584)); //true
+console.log(isFibonachi(6546)); //false
+console.log(isFibonachi(5)); //true
